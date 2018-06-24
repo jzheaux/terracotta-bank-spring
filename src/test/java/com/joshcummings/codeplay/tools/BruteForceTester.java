30747fdb2a6cdbd73cef43995b06ede065fae742
Josh Cummings <josh.cummings@gmail.com>
2018-06-23 22:18:41 -0600

    Missing copyright

--- src/test/java/com/joshcummings/codeplay/tools/BruteForceTester.java
+++ src/test/java/com/joshcummings/codeplay/tools/BruteForceTester.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2018 Josh Cummings
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.joshcummings.codeplay.tools;
 
 import com.joshcummings.codeplay.terracotta.testng.HttpSupport;
@@ -12,7 +27,7 @@ import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.List;
 
-public class BruteForceLogin {
+public class BruteForceTester {
 	private static HttpSupport http = new HttpSupport();
 	private static Base64.Encoder encoder = Base64.getEncoder();
 
