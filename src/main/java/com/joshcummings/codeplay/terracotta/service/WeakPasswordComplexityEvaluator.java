36d5c4772d8f6effe06ec38205c3b58e8aa0e805
Josh Cummings <josh.cummings@gmail.com>
2018-08-04 16:38:14 -0600

    Separated special characters out

--- src/main/java/com/joshcummings/codeplay/terracotta/service/WeakPasswordComplexityEvaluator.java
+++ src/main/java/com/joshcummings/codeplay/terracotta/service/WeakPasswordComplexityEvaluator.java
@@ -3,11 +3,13 @@ package com.joshcummings.codeplay.terracotta.service;
 import java.util.regex.Pattern;
 
 public class WeakPasswordComplexityEvaluator implements PasswordComplexityEvaluator {
+	private static final String SPECIAL_CHARACTERS = "!@#^";
+
 	private static final Pattern HAS_LOWER = Pattern.compile("[a-z]");
 	private static final Pattern HAS_UPPER = Pattern.compile("[A-Z]");
 	private static final Pattern HAS_NUMBER = Pattern.compile("[0-9]");
-	private static final Pattern HAS_SPECIAL = Pattern.compile("[!@#^]");
-	private static final Pattern HAS_ONLY = Pattern.compile("[a-zA-Z0-9!@#^]{6,20}");
+	private static final Pattern HAS_SPECIAL = Pattern.compile("[" + SPECIAL_CHARACTERS + "]");
+	private static final Pattern HAS_ONLY = Pattern.compile("[a-zA-Z0-9" + SPECIAL_CHARACTERS + "]{6,20}");
 
 	@Override
 	public boolean evaluate(String password) {
