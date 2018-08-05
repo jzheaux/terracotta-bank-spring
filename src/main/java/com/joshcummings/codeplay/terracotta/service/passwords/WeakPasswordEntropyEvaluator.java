779f5c668004ae3d82681cdaecd753797736dadb
Josh Cummings <josh.cummings@gmail.com>
2018-08-04 18:23:58 -0600

    Password Complexity - Special Characters, Length
    Passwords are hashed in the end, so what the user puts in their
    password is typically harmless to the underlying system storing it.
    Because they are hashed, the contents become a predictable
    character set and consistent length.
    
    So, users should be allowed a wider range of special characters than
    many sites allow and longer passwords than allowed as well.

--- src/main/java/com/joshcummings/codeplay/terracotta/service/passwords/WeakPasswordEntropyEvaluator.java
+++ src/main/java/com/joshcummings/codeplay/terracotta/service/passwords/WeakPasswordEntropyEvaluator.java
@@ -3,13 +3,13 @@ package com.joshcummings.codeplay.terracotta.service.passwords;
 import java.util.regex.Pattern;
 
 public class WeakPasswordEntropyEvaluator implements PasswordEntropyEvaluator {
-	private static final String SPECIAL_CHARACTERS = "!@#^";
+	private static final String SPECIAL_CHARACTERS = " !\"#\\$%&'\\(\\)\\*\\+,\\-\\./:;<=>\\?@\\[\\\\\\]\\^_`\\{\\|\\}~";;
 
 	private static final Pattern HAS_LOWER = Pattern.compile("[a-z]");
 	private static final Pattern HAS_UPPER = Pattern.compile("[A-Z]");
 	private static final Pattern HAS_NUMBER = Pattern.compile("[0-9]");
 	private static final Pattern HAS_SPECIAL = Pattern.compile("[" + SPECIAL_CHARACTERS + "]");
-	private static final Pattern HAS_ONLY = Pattern.compile("[a-zA-Z0-9" + SPECIAL_CHARACTERS + "]{6,20}");
+	private static final Pattern HAS_ONLY = Pattern.compile("[a-zA-Z0-9" + SPECIAL_CHARACTERS + "]{8,128}");
 
 	@Override
 	public Evaluation evaluate(String password) {
