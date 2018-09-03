629beb5cdca3d8ce34edb4f5b64f4435ba0572ee
Josh Cummings <josh.cummings@gmail.com>
2018-09-03 10:46:53 -0600

    Authentication Logging - Authentication Events
    We want to log whenever authentication events pass or fail, and why.
    We also want to log whenever authorization events pass or fail, and
    whenever sensitive data access requests pass or fail, and why.
    
    Note that in this commit, all of these are configured to be on, but it
    would likely be necessary to add additional domain-specific logic for
    authorization-success event logging to be informative.

--- src/main/java/com/joshcummings/codeplay/terracotta/config/SecurityConfiguration.java
+++ src/main/java/com/joshcummings/codeplay/terracotta/config/SecurityConfiguration.java
@@ -4,15 +4,30 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
+import javax.servlet.Filter;
+
 import com.joshcummings.codeplay.terracotta.model.User;
 import com.joshcummings.codeplay.terracotta.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.access.event.AuthorizationFailureEvent;
+import org.springframework.security.access.event.AuthorizedEvent;
+import org.springframework.security.access.intercept.aopalliance.MethodSecurityInterceptor;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,12 +37,15 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.session.ChangeSessionIdAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.CompositeSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.stereotype.Component;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -47,6 +65,63 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and()
 			.sessionManagement()
 				.sessionAuthenticationStrategy(sessionAuthenticationStrategy());
+	}
+
+	@Bean
+	public DefaultAuthenticationEventPublisher authenticationEventPublisher() {
+		return new DefaultAuthenticationEventPublisher();
+	}
+
+	@Component
+	static class LoggerConfiguration {
+		private final Logger logger = LoggerFactory.getLogger("authentication-event");
+
+		@EventListener
+		public void logAuthenticationFailure(AbstractAuthenticationFailureEvent failure) {
+			Authentication attempt = failure.getAuthentication();
+			logger.info("Authentication failed for " + attempt.getName() +
+					" because " + failure.getException().getMessage());
+		}
+
+		@EventListener
+		public void logAuthenticationSuccess(AuthenticationSuccessEvent success) {
+			Authentication token = success.getAuthentication();
+			logger.info("Authentication success for " + token.getName());
+		}
+
+		@EventListener
+		public void logAuthorizationFailure(AuthorizationFailureEvent failure) {
+			Authentication attempt = failure.getAuthentication();
+			logger.info("Authorization failure for " + attempt.getName() +
+					" having authorities " + attempt.getAuthorities() +
+					" because " + failure.getAccessDeniedException().getMessage());
+		}
+
+		@EventListener
+		public void logAuthorizationSuccess(AuthorizedEvent success) {
+			Authentication token = success.getAuthentication();
+			logger.info("Authorization success for " + token.getName() +
+					" having authorities " + token.getAuthorities() +
+					" to resource " + success.getSource());
+		}
+
+		@Autowired
+		ApplicationContext context;
+
+		private <T extends Filter> T getFilter(Class<T> filterClass) {
+			FilterChainProxy filterChain = context.getBean(FilterChainProxy.class);
+			return (T)filterChain.getFilters("/").stream()
+					.filter(filterClass::isInstance)
+					.findFirst().orElse(null);
+		}
+
+		@EventListener
+		public void handle(ContextRefreshedEvent event) {
+			FilterSecurityInterceptor fsi = getFilter(FilterSecurityInterceptor.class);
+			fsi.setPublishAuthorizationSuccess(true);
+			MethodSecurityInterceptor msi = context.getBean(MethodSecurityInterceptor.class);
+			msi.setPublishAuthorizationSuccess(true);
+		}
 	}
 
 	@Bean
